8f4e781814e0b4b3f24b321906b19b6d39876f7c
Akhil846 <akhilvemula18@gmail.com>
2024-06-16 21:14:04 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/growth_purple_assignment/",
-  plugins: [react()],
+   plugins: [react()],
 })
